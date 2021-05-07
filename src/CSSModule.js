d289cf75d7c6895f183628daad60df4caa217228
engineer135 <everafterk@naver.com>
2021-05-08 07:08:19 +0900

    CSSModule 적용해보기

---
+++ src/CSSModule.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './CSSModule.module.css';
+
+const CSSModule = () => {
+    return (
+        <div className={styles.wrapper}>
+            안녕 나는 <span className="something">css 모듈이야</span>
+        </div>
+    );
+};
+
+export default CSSModule;
