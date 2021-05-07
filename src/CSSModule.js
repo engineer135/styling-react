ffdce5b52ecbdba530171433beb8009ae8551cde
engineer135 <everafterk@naver.com>
2021-05-08 07:15:44 +0900

    템플릿 리터럴로 스타일 여러개 적용

--- src/CSSModule.js
+++ src/CSSModule.js
@@ -3,7 +3,7 @@ import styles from './CSSModule.module.css';
 
 const CSSModule = () => {
     return (
-        <div className={styles.wrapper}>
+        <div className={`${styles.wrapper} ${styles.inverted}`}>
             안녕 나는 <span className="something">css 모듈이야</span>
         </div>
     );
